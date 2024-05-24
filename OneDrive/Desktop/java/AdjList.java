b4b7f51481d98a40bfd332849110236db748105d
biswashdaiii <biswashghimire97@gmail.com>
2024-05-24 09:20:02 +0545

    sir list

--- OneDrive/Desktop/java/AdjList.java
+++ OneDrive/Desktop/java/AdjList.java
@@ -3,58 +3,38 @@ import java.util.List;
 
 public class AdjList {
     int v;
-    singlylinkedlist[] adjList;
-    public AdjList(int v){
-        this.v=v;
-        adjList=new singlylinkedlist[v];
-        for(int i =0 ; i<v; i++){
-            adjList[i]= new singlylinkedlist();
+    singlylinkedlist [] adjlist;
+    public AdjList(int v) {
+        this.v = v;
+        adjlist = new singlylinkedlist[v];
+        for (int i = 0; i < v; i++) {
+            adjlist[i] = new singlylinkedlist();
         }
-        
     }
 
-    void addEdge(int source,int destination){
-        adjList[source].addNode(destination);
-        adjList[destination].addNode(source);
-
+    void addEdge(int source, int destination) {
+        adjlist[source].addNode(destination);
+        adjlist[destination].addNode(source);
     }
-     void printgraph(){
-        for (int i=0; i<v; i++){
-            System.out.print(i+"is connected");
-            singlylinkedlist.Node current=adjList[i].head;
-            while(current!=null){
-                System.out.print(current.data+"");
+
+    void printGraph(){
+        for (int i = 0; i < v; i++) {
+            System.out.print(i +" is connected to ");
+            singlylinkedlist.Node current=adjlist[i].head;
+            while (current!=null){
+                System.out.print(current.data+" ");
                 current=current.next;
             }
         }
     }
-    List<Integer>getAdjNodes(int i){
-        List<Integer>List=new ArrayList<Integer>();
-         singlylinkedlist.Node current=adjList[i].head;
-            while(current!=null){
-                System.out.print(current.data+"");
-                current=current.next;
 
-    }
-    return List;
-
-    void BFS (int source){
-        Queues q= new Queues(v);
-        boolean[] visited=new boolean[v];
-        visited[source]=true;
-        q.enqueue(source);
-        while(!q.isEmpty()){
-
-            int U=q.dequeue();
-            System.out.println(u);
-            for(int j =0; j<v;j++);
-            if(matrix[u][j]==1){
-                if(!visited[j]){
-                    q.enqueue(j);
-                    visited[j]=true
-                }
-            }
+    List<Integer> getAdjNodes(int i){
+        List<Integer> list = new ArrayList<Integer>();
+        singlylinkedlist.Node current=adjlist[i].head;
+        while (current!=null){
+            list.add(current.data);
+            current=current.next;
         }
+        return list;
     }
-}
 }
--- OneDrive/Desktop/java/AdjMatrix.java
+++ OneDrive/Desktop/java/AdjMatrix.java
@@ -5,55 +5,82 @@ public class AdjMatrix {
     int v;
     int [][] matrix;
     AdjMatrix(int v){
-        this.v= v;
+        this.v=v;
         matrix=new int[v][v];
     }
 
-    void addEdge(int source,int destination){
+    void addEdge(int source, int destination){
         matrix[source][destination]=1;
         matrix[destination][source]=1;
     }
-    void printgraph(){
-        
-       
-        for (int i=0;i<v;i++){
-            System.out.print(i+"is connected to");
+
+    void printGraph(){
+        for(int i=0; i<v; i++){
+            System.out.print(i +" is connected to ");
             for(int j=0; j<v; j++){
                 if(matrix[i][j]!=0){
-                    System.out.print(j+"");
+                    System.out.print(j+" ");
                 }
-
             }
             System.out.println();
         }
-
     }
-    List<Integer> getAdjNodes(int i){
-        List<Integer> List=new ArrayList<>();
+
+    List<Integer> getAdjNOdes(int i){
+        List<Integer> list=new ArrayList<>();
         for(int j=0; j<v; j++){
             if(matrix[i][j]!=0){
-                List.add(j);
-            return List;
-            }
-
-            void dfs(boolean[] visited,int source){
-                
-            }
-            void depthFirstSearch(int source){
-                boolean[] visited=new boolean[v];
-                dfs(visited,source);
+                list.add(j);
             }
         }
-   public static void main(String[] args) {
-    AdjMatrix adj = new AdjMatrix(6);
-    adj.addEdge(0, 1);
-    adj.addEdge(0, 2);  
-    adj.addEdge(0, 5);
-    adj.addEdge(1, 3);
-    adj.addEdge(2,3);
-    adj.addEdge(2, 5);
-    adj.addEdge(3, 4);
-    adj.addEdge(4, 5);
-    adj.printgraph();
-   } 
+        return list;
+    }
+
+    void BFS(int source){
+        Queues q=new Queues(v);
+        boolean [] visited=new boolean[v];
+        visited[source]=true;
+        q.enqueue(source);
+        while(!q.isEmpty()){
+            int u=q.dequeue();
+            System.out.println(u);
+            for(int j=0; j<v; j++){
+                if(matrix[u][j]!=0){
+                    if(!visited[j]){
+                        q.enqueue(j);
+                        visited[j]=true;
+                    }
+                }
+            }
+        }
+    }
+    void dfs(boolean [] visited,int source){
+       visited[source]=true;
+        System.out.println(source);
+        for(int j=0; j<v; j++){
+            if(matrix[source][j]!=0){
+                if(!visited[j]){
+                    dfs(visited,j);
+                }
+            }
+        }
+    }
+
+    void depthFirstSearch(int source){
+        boolean [] visited=new boolean[v];
+        dfs(visited,source);
+    }
+
+    public static void main(String[] args) {
+        AdjMatrix adj=new AdjMatrix(6);
+        adj.addEdge(0,1);
+        adj.addEdge(0,2);
+        adj.addEdge(0,5);
+        adj.addEdge(1,3);
+        adj.addEdge(2,3);
+        adj.addEdge(2,5);
+        adj.addEdge(3,4);
+        adj.addEdge(4,5);
+        adj.printGraph();
+    }
 }
